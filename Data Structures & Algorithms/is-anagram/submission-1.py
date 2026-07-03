a2aa0b965ebbc12e91b20a3baa759234e61d3274
Alan Tapper <76759811+alan-tapper@users.noreply.github.com>
2026-07-03 13:08:13 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # if len(s) != len(t):
+        #     return False
+        # occ_s, occ_t = {}, {}
+        # for i in range(len(s)):
+            
+        #     if s[i] not in occ_s:
+        #         occ_s[s[i]] = 0
+        #     occ_s[s[i]] += 1
+            
+        #     if t[i] not in occ_t:
+        #         occ_t[t[i]] = 0
+        #     occ_t[t[i]] += 1
+        
+        # s_set = set([c for c in s])
+        # for c in s_set:
+        #     if occ_s != occ_t:
+        #         return False
+        # return True
+
+        return sorted(s) == sorted(t)
